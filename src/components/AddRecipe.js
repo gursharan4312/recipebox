0794d2570697980fc8dbc5b48323694135e66e2a
Gursharan Singh <gursharansingh4312@gmail.com>
2019-01-17 10:42:51 -0800

    add recipe modal added

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -13,11 +13,14 @@ class AddRecipe extends Component{
           <div className="modalBody">
             <form>
               <label htmlFor="name">Name of recipe:</label><br/>
-              <input type="text" id="name"/><br/>
+              <input type="text" id="name"/><br/><br/>
+
               <label htmlFor="ingredientInput">Ingredients:</label><br/>
-              <textarea id="ingredientInput"></textarea><br/>
+              <textarea id="ingredientInput" rows="3" cols="40" placeholder="*seperate each ingredient with '\':&#10;Milk \ 2 eggs \ salt"></textarea><br/><br />
+
               <label htmlFor="directionInput">Directions:</label><br/>
-              <textarea id="directionInput"></textarea><br/>
+              <textarea id="directionInput" rows="5" cols="40" placeholder="* seperate each step with '\': &#10;add milk \&#10;add eggs \&#10;add salt \"></textarea><br/>
+
               <input className="btn" type="submit" value="Add" style={{}}/>
               <Link to="/" className="btn">Cancel</Link>
             </form>
